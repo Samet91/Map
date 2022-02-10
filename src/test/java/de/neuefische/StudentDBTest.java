8d747ba0a4112283b21eb3693df2bfd226587682
Samet Uslu <samet_u@hotmail.de>
2022-02-10 18:38:10 +0100

    create test with two Students to add in my DB

--- src/test/java/de/neuefische/StudentDBTest.java
+++ src/test/java/de/neuefische/StudentDBTest.java
@@ -23,6 +23,21 @@ class StudentDBTest {
 
     }
 
+    @Test
+    void shouldPushTwoNewStudentInDB() {
 
+        Student student = new Student(1, "Samet", "Uslu", 31);
+        Student student2 = new Student(2, "das", "dies", 31);
+
+        Map<Integer, Student> mapStudent = new HashMap<>();
+        mapStudent.put(1, student);
+        mapStudent.put(2, student2);
+
+        StudentDB studentDB = new StudentDB();
+        studentDB.setMapOfStudents(mapStudent);
+
+        assertEquals(2, studentDB.getMapOfStudents().size());
+
+    }
 
 }
